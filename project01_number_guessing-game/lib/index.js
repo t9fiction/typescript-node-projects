b2473cc45dea575c355cfd9c7fee5483b0bbc0b7
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-11-24 01:54:21 +0500

    initial

--- project01_number_guessing-game/lib/index.js
+++ project01_number_guessing-game/lib/index.js
@@ -1,2 +1,4 @@
-console.log("Hello World");
+#!/usr/bin/env node
+let magicNum = Math.floor(Math.random() * 100);
+console.log(magicNum);
 export {};
--- project01_number_guessing-game/src/index.tsx
+++ project01_number_guessing-game/src/index.tsx
@@ -1,1 +1,41 @@
-console.log("Hello World")
+#!/usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { createSpinner } from "nanospinner";
+import chalkAnimation from "chalk-animation";
+
+let magicNum = Math.floor(Math.random()*100)
+
+const log = console.log;
+
+const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+
+async function gussNum() {
+    console.clear();
+    log("\n");
+    log(chalk.bgGreen("Calculator by Sohail Ishaque : PIAIC105167"));
+    log("\n");
+
+    //Asking for user Name
+  let user = await inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "name",
+      message: chalk.green("What is your Name ?"),
+      default: "User",
+    },
+  ])
+  .then(
+    (result) => {
+      log(chalk.bgGreen("\n Welcome ", chalk.red(result.name), "...\n"));
+      return result.name;
+    },
+    (error) => {
+      log("error : ", error);
+    }
+  );
+
+}
+
+console.log(magicNum)
