0e7fc989255927c2413a830dc43fb37ed96ee823
Sohail Ishaque <sohail.sohailishaq@gmail.com>
2022-11-26 18:48:27 +0500

    game working fine

--- project01_number_guessing-game/lib/index.js
+++ project01_number_guessing-game/lib/index.js
@@ -1,4 +1,134 @@
 #!/usr/bin/env node
-let magicNum = Math.floor(Math.random() * 100);
-console.log(magicNum);
-export {};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import inquirer from "inquirer";
+import chalk from "chalk";
+import { createSpinner } from "nanospinner";
+import chalkAnimation from "chalk-animation";
+let magicNum = Math.floor(Math.random() * 10) + 1;
+let num1 = 0;
+const log = console.log;
+const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
+//---- Start of  Main Function ----
+function guessNumber() {
+    return __awaiter(this, void 0, void 0, function* () {
+        console.clear();
+        log("\n");
+        // log("\n");
+        // console.log(magicNum);
+        yield rainbowEffect();
+        yield myName();
+        yield makingGuess();
+    });
+}
+//---- End of guessNumber ----
+// ---- Rainbow Effect ----
+function rainbowEffect() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let rainbow = chalkAnimation.rainbow(`\t"Guessing the Number"\n 
+    by Sohail Ishaque : PIAIC105167`);
+        rainbow.start(); // Animation resumes
+        yield sleep();
+        // setTimeout(() => {
+        //   rainbow.stop(); // Animation stops
+        // }, 5000);
+        // setTimeout(() => {
+        // rainbow.start(); // Animation resumes
+        // }, 200);
+        log("\n");
+    });
+}
+//-----Asking for user Name function-----
+function myName() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let user = yield inquirer
+            .prompt([
+            {
+                type: "input",
+                name: "name",
+                message: chalk.green("What is your Name ?"),
+                default: "User",
+            },
+        ])
+            .then((result) => {
+            log(chalk.bgCyan("\n Welcome ", chalk.red(result.name), "...\n"));
+            return result.name;
+        }, (error) => {
+            log("error : ", error);
+        });
+    });
+}
+// ----End of function myName----
+// guessing
+function subGuess() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let guess = yield inquirer
+            .prompt({
+            type: "number",
+            name: "tukka",
+            message: "Make another Guess =>  ",
+            default: "0",
+        })
+            .then((result) => {
+            num1 = result.tukka;
+        });
+        return ifChecking();
+    });
+}
+function ifChecking() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const spinner = createSpinner("Checking answer...\n").start();
+        yield sleep();
+        if (num1 >= 0 && num1 < 10) {
+            if (num1 == magicNum) {
+                spinner.success({
+                    text: chalk.blue(`Hurrah, You have guessed the number`),
+                });
+            }
+            else if (magicNum <= 5) {
+                spinner.error({
+                    text: chalk.green(`Your guess is not correct. ` + chalk.blueBright("Hint!") + ` Try something between 1 - 5`),
+                });
+                subGuess();
+            }
+            else {
+                spinner.error({
+                    text: chalk.green(`Your guess is not correct. ` + chalk.blueBright("Hint!") + ` Try something between 6 - 10`),
+                });
+                subGuess();
+            }
+        }
+        else {
+            console.log(num1);
+            console.log(magicNum);
+            spinner.error({
+                text: chalk.green(`Wrong input`),
+            });
+            subGuess();
+        }
+    });
+}
+// ----- Guess the num -----
+function makingGuess() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let guess = yield inquirer
+            .prompt({
+            type: "number",
+            name: "tukka",
+            message: "Guess the number between 1 to 10 =>  ",
+            default: "0",
+        })
+            .then((result) => {
+            num1 = result.tukka;
+        });
+        yield ifChecking();
+    });
+}
+guessNumber();
--- project01_number_guessing-game/src/index.tsx
+++ project01_number_guessing-game/src/index.tsx
@@ -4,38 +4,131 @@ import chalk from "chalk";
 import { createSpinner } from "nanospinner";
 import chalkAnimation from "chalk-animation";
 
-let magicNum = Math.floor(Math.random()*100)
+let magicNum = Math.floor(Math.random() * 10) + 1;
 
+let num1 = 0;
 const log = console.log;
 
 const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
-async function gussNum() {
-    console.clear();
-    log("\n");
-    log(chalk.bgGreen("Calculator by Sohail Ishaque : PIAIC105167"));
-    log("\n");
+//---- Start of  Main Function ----
+async function guessNumber() {
+  console.clear();
+  log("\n");
+  // log("\n");
 
-    //Asking for user Name
-  let user = await inquirer
-  .prompt([
-    {
-      type: "input",
-      name: "name",
-      message: chalk.green("What is your Name ?"),
-      default: "User",
-    },
-  ])
-  .then(
-    (result) => {
-      log(chalk.bgGreen("\n Welcome ", chalk.red(result.name), "...\n"));
-      return result.name;
-    },
-    (error) => {
-      log("error : ", error);
-    }
+  // console.log(magicNum);
+  await rainbowEffect();
+  await myName();
+  await makingGuess();
+}
+//---- End of guessNumber ----
+
+// ---- Rainbow Effect ----
+async function rainbowEffect() {
+  let rainbow = chalkAnimation.rainbow(
+    `\t"Guessing the Number"\n 
+    by Sohail Ishaque : PIAIC105167`
   );
 
+  rainbow.start(); // Animation resumes
+  await sleep();
+  // setTimeout(() => {
+  //   rainbow.stop(); // Animation stops
+  // }, 5000);
+
+  // setTimeout(() => {
+  // rainbow.start(); // Animation resumes
+  // }, 200);
+  log("\n");
 }
 
-console.log(magicNum)
+//-----Asking for user Name function-----
+async function myName() {
+  let user = await inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "name",
+        message: chalk.green("What is your Name ?"),
+        default: "User",
+      },
+    ])
+    .then(
+      (result) => {
+        log(chalk.bgCyan("\n Welcome ", chalk.red(result.name), "...\n"));
+        return result.name;
+      },
+      (error) => {
+        log("error : ", error);
+      }
+    );
+}
+// ----End of function myName----
+
+// guessing
+async function subGuess() {
+  let guess = await inquirer
+    .prompt({
+      type: "number",
+      name: "tukka",
+      message: "Make another Guess =>  ",
+      default: "0",
+    })
+    .then((result) => {
+      num1 = result.tukka;
+    });
+  return ifChecking();
+}
+
+async function ifChecking() {
+  const spinner = createSpinner("Checking answer...\n").start();
+  await sleep();
+
+  if (num1 >= 0 && num1 < 10) {
+    if (num1 == magicNum) {
+      spinner.success({
+        text: chalk.blue(`Hurrah, You have guessed the number`),
+      });
+    } else if (magicNum <= 5) {
+      spinner.error({
+        text: chalk.green(
+          `Your guess is not correct. `+ chalk.blueBright("Hint!") +` Try something between 1 - 5`
+        ),
+      });
+      subGuess();
+    } else {
+      spinner.error({
+        text: chalk.green(
+          `Your guess is not correct. `+ chalk.blueBright("Hint!") +` Try something between 6 - 10`
+        ),
+      });
+      subGuess();
+    }
+  } else {
+    console.log(num1);
+    console.log(magicNum);
+    spinner.error({
+      text: chalk.green(`Wrong input`),
+    });
+    subGuess();
+  }
+}
+
+// ----- Guess the num -----
+async function makingGuess() {
+  let guess = await inquirer
+    .prompt({
+      type: "number",
+      name: "tukka",
+      message: "Guess the number between 1 to 10 =>  ",
+      default: "0",
+    })
+    .then((result) => {
+      num1 = result.tukka;
+    });
+
+  await ifChecking();
+}
+
+guessNumber();
